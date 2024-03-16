042aeba3b1cd8b543d770416473292c343fe2113
Shoaib <shoaib.ali@uipath.com>
2024-03-15 20:32:56 -0500

    init

---
+++ CodedMain1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using UiPath.CodedWorkflows;
+using UiPath.Core;
+using UiPath.Core.Activities.Storage;
+using UiPath.Orchestrator.Client.Models;
+
+namespace CodedOStst2
+{
+    public class CodedMain1 : CodedWorkflow
+    {
+        [Workflow]
+        public void Execute()
+        {
+            // To start using services, use IntelliSense (CTRL + Space) to discover the available services:
+            // e.g. system.GetAsset(...)
+
+            // For accessing UI Elements from Object Repository, you can use the Descriptors class e.g:
+            // var screen = uiAutomation.Open(Descriptors.MyApp.FirstScreen);
+            // screen.Click(Descriptors.MyApp.FirstScreen.SettingsButton);
+            Log("test1");
+        }
+    }
+}
